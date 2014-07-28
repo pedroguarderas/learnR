71e965c3328767d57c5f5323d4d1c8663b83c964
aju <aju@te.ceaaces>
2014-07-28 15:43:52 -0500

    Adicionando encabezado.

--- R_base/session_1.R
+++ R_base/session_1.R
@@ -1,4 +1,15 @@
-# ------------------------------------------------------------------------------
+# __________________________________________________________________________________________________
+# 
+# autor: Pedro Guarderas
+# email: ajusworkopensource@gmail.com
+# file: session_1.R
+# 
+# This program is free software; you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software Foundation; 
+# either version 2 of the License, or (at your option) any later version.
+# __________________________________________________________________________________________________
+# 
+
 # Tutorial básico de R
 # R is un lenguage de programación libre y un entorno para estadística computacional.
 # R is una implementación del lenguage de programación S.
--- R_base/session_2.R
+++ R_base/session_2.R
@@ -1,3 +1,15 @@
+# __________________________________________________________________________________________________
+# 
+# autor: Pedro Guarderas
+# email: ajusworkopensource@gmail.com
+# file: session_2.R
+# 
+# This program is free software; you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software Foundation; 
+# either version 2 of the License, or (at your option) any later version.
+# __________________________________________________________________________________________________
+# 
+
 #___________________________________________________________________________________________________
 # Simulación de regresión
 n<-100
