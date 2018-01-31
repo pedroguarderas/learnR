4e97a072a723e5dc698139672cc987d9369a50e9
pedroguarderas <pedro.guarderas@webrisko.com>
2018-01-31 14:36:57 -0500

    Reducing commentaries.

--- R_base/session_01.R
+++ R_base/session_01.R
@@ -1,14 +1,7 @@
 # --------------------------------------------------------------------------------------------------
-# 
 # autor: Pedro Guarderas
 # email: pedro.felipe.guarderas@gmail.com
-# file: session_1.R
-# 
-# This program is free software; you can redistribute it and/or modify it under the 
-# terms of the GNU General Public License as published by the Free Software Foundation; 
-# either version 2 of the License, or (at your option) any later version.
 # --------------------------------------------------------------------------------------------------
-
 
 # Tutorial básico de R
 # R is un lenguage de programación libre y un entorno para estadística computacional.
