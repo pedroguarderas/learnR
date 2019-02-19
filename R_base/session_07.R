6025b522c099e6c9d58c3cd940942b7633322fa7
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2019-02-19 12:23:15 -0500

    Quitando comentarios

--- R_base/session_07.R
+++ R_base/session_07.R
@@ -1,15 +1,3 @@
-# __________________________________________________________________________________________________
-# 
-# autor: Pedro Guarderas
-# email: ajusworkopensource@gmail.com
-# file: session_7.R
-# 
-# This program is free software; you can redistribute it and/or modify it under the 
-# terms of the GNU General Public License as published by the Free Software Foundation; 
-# either version 2 of the License, or (at your option) any later version.
-# __________________________________________________________________________________________________
-# 
-
 #___________________________________________________________________________________________________
 # Estudio de la desigualdad de Chebyshev P( | X - E[X] | > t ) <= Var[X] / t^2
 m<-1000
--- R_special/curvature.R
+++ R_special/curvature.R
@@ -1,14 +1,3 @@
-# __________________________________________________________________________________________________
-# 
-# author: Pedro Guarderas
-# email: ajusworkopensource@gmail.com
-# file: curvature.R
-# 
-# This program is free software; you can redistribute it and/or modify it under the 
-# terms of the GNU General Public License as published by the Free Software Foundation; 
-# either version 2 of the License, or (at your option) any later version.
-# __________________________________________________________________________________________________
-
 #___________________________________________________________________________________________________
 library( ggplot2 )
 
